6d6e61f64de17ccddc931ae3cec1a333d8045840
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-21 20:18:13 -0300

    Feat: implement method calcular() in CalculadoraDeDescontos

--- src/com/ndrewcoding/loja/desconto/CalculadoraDeDescontos.java
+++ src/com/ndrewcoding/loja/desconto/CalculadoraDeDescontos.java
@@ -1,4 +1,17 @@
 package com.ndrewcoding.loja.desconto;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class CalculadoraDeDescontos {
+
+    public BigDecimal calcular(Orcamento orcamento) {
+        if (orcamento.getQuantidadeItens() > 5) {
+            return orcamento.getValor().multiply(new BigDecimal("0.1"));
+        }
+
+        return BigDecimal.ZERO;
+    }
+
 }

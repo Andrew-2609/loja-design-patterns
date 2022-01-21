2ca4cf7b27e925e616259a416a66d24ab1943314
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 21:06:53 -0300

    Feat: create method calcular() in CalculadoraDeImpostos

--- src/com/ndrewcoding/loja/imposto/CalculadoraDeImpostos.java
+++ src/com/ndrewcoding/loja/imposto/CalculadoraDeImpostos.java
@@ -1,4 +1,20 @@
 package com.ndrewcoding.loja.imposto;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class CalculadoraDeImpostos {
+
+    public BigDecimal calcular(Orcamento orcamento, TipoDeImposto tipoDeImposto) {
+        switch (tipoDeImposto) {
+            case ICMS:
+                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+            case ISS:
+                return orcamento.getValor().multiply(new BigDecimal("0.06"));
+            default:
+                return BigDecimal.ZERO;
+        }
+    }
+
 }

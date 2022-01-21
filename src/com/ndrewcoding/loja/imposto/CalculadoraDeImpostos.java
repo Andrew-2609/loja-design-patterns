dba47aa95eaa614784a4f035e49ddfebc47be7de
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 21:16:11 -0300

    Refactor: correct the new implementation of calcular() method of CalculadoraDeImpostos

--- src/com/ndrewcoding/loja/imposto/CalculadoraDeImpostos.java
+++ src/com/ndrewcoding/loja/imposto/CalculadoraDeImpostos.java
@@ -7,14 +7,7 @@ import java.math.BigDecimal;
 public class CalculadoraDeImpostos {
 
     public BigDecimal calcular(Orcamento orcamento, TipoDeImposto tipoDeImposto) {
-        switch (tipoDeImposto) {
-            case ICMS:
-                return orcamento.getValor().multiply(new BigDecimal("0.1"));
-            case ISS:
-                return orcamento.getValor().multiply(new BigDecimal("0.06"));
-            default:
-                return BigDecimal.ZERO;
-        }
+        return tipoDeImposto.calcular(orcamento);
     }
 
 }

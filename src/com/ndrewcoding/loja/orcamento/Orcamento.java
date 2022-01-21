e8248a4b1237b2f23839d4098290ed0b99b8063a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 21:02:04 -0300

    Feat: add basic structure of Orcamento

--- src/com/ndrewcoding/loja/orcamento/Orcamento.java
+++ src/com/ndrewcoding/loja/orcamento/Orcamento.java
@@ -1,4 +1,17 @@
 package com.ndrewcoding.loja.orcamento;
 
+import java.math.BigDecimal;
+
 public class Orcamento {
+
+    private final BigDecimal valor;
+
+    public Orcamento(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
 }

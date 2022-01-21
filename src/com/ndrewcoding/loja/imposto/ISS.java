35752545eeadf14ac946f3f1e413bb8e0c1dbb2f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 21:15:27 -0300

    Feat: implement TipoDeImposto in ISS

--- src/com/ndrewcoding/loja/imposto/ISS.java
+++ src/com/ndrewcoding/loja/imposto/ISS.java
@@ -4,8 +4,9 @@ import com.ndrewcoding.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class ISS {
+public class ISS implements TipoDeImposto {
 
+    @Override
     public BigDecimal calcular(Orcamento orcamento) {
         return orcamento.getValor().multiply(new BigDecimal("0.06"));
     }

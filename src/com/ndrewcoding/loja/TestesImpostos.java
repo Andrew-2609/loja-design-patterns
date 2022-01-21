c28cfa2976c037e06b3027a3883c64d4cbb1d513
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-20 21:10:11 -0300

    Feat: add basic implementation of TesteImpostos

--- src/com/ndrewcoding/loja/TestesImpostos.java
+++ src/com/ndrewcoding/loja/TestesImpostos.java
@@ -1,4 +1,18 @@
 package com.ndrewcoding.loja;
 
+import com.ndrewcoding.loja.imposto.CalculadoraDeImpostos;
+import com.ndrewcoding.loja.imposto.TipoDeImposto;
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class TestesImpostos {
+    public static void main(String[] args) {
+
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoDeImposto.ICMS));
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoDeImposto.ISS));
+
+    }
 }

00fccf48df13e79980b7919d14630b1a14a183dd
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-21 20:20:04 -0300

    Feat: add basic implementation of TestesDescontos

--- src/com/ndrewcoding/loja/TestesDescontos.java
+++ src/com/ndrewcoding/loja/TestesDescontos.java
@@ -1,4 +1,16 @@
 package com.ndrewcoding.loja;
 
+import com.ndrewcoding.loja.desconto.CalculadoraDeDescontos;
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class TestesDescontos {
+    public static void main(String[] args) {
+
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 6);
+        CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+        System.out.println(calculadoraDeDescontos.calcular(orcamento));
+
+    }
 }

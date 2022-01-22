955c12717c9cf3c0c0ee7ccbdf645efbe12d4151
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-21 21:01:01 -0300

    Feat: make DescontoParaOrcamentoComMaisDeCincoItens a subclass of Desconto

--- src/com/ndrewcoding/loja/desconto/DescontoParaOrcamentoComMaisDeCincoItens.java
+++ src/com/ndrewcoding/loja/desconto/DescontoParaOrcamentoComMaisDeCincoItens.java
@@ -4,8 +4,13 @@ import com.ndrewcoding.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class DescontoParaOrcamentoComMaisDeCincoItens {
+public class DescontoParaOrcamentoComMaisDeCincoItens extends Desconto {
 
+    public DescontoParaOrcamentoComMaisDeCincoItens(Desconto proximoDesconto) {
+        super(proximoDesconto);
+    }
+
+    @Override
     public BigDecimal calcular(Orcamento orcamento) {
         if (orcamento.getQuantidadeItens() > 5) {
             return orcamento.getValor().multiply(new BigDecimal("0.1"));

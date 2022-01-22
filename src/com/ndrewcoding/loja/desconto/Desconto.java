82fe1fba9fe9295de0a32df240dd9d79c1b6a622
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-21 21:00:18 -0300

    Feat: implement basic structure of Desconto

--- src/com/ndrewcoding/loja/desconto/Desconto.java
+++ src/com/ndrewcoding/loja/desconto/Desconto.java
@@ -1,4 +1,17 @@
 package com.ndrewcoding.loja.desconto;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public abstract class Desconto {
+
+    protected Desconto proximoDesconto;
+
+    public Desconto(Desconto proximoDesconto) {
+        this.proximoDesconto = proximoDesconto;
+    }
+
+    public abstract BigDecimal calcular(Orcamento orcamento);
+
 }

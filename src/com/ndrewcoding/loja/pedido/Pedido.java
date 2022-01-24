dd0efb743855b43d1cfa1880af7680c958ebcbb3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:48:57 -0300

    Feat: create basic structure of Pedido

--- src/com/ndrewcoding/loja/pedido/Pedido.java
+++ src/com/ndrewcoding/loja/pedido/Pedido.java
@@ -1,4 +1,31 @@
 package com.ndrewcoding.loja.pedido;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.time.LocalDateTime;
+
 public class Pedido {
+
+    private final String cliente;
+    private final LocalDateTime data;
+    private final Orcamento orcamento;
+
+    public Pedido(String cliente, LocalDateTime data, Orcamento orcamento) {
+        this.cliente = cliente;
+        this.data = data;
+        this.orcamento = orcamento;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public Orcamento getOrcamento() {
+        return orcamento;
+    }
+    
 }

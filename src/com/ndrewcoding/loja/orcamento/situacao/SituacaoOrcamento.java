f560dade66888dd50881eca2b4bf90315ce08284
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:10:52 -0300

    Feat: create core methods of SituacaoOrcamento

--- src/com/ndrewcoding/loja/orcamento/situacao/SituacaoOrcamento.java
+++ src/com/ndrewcoding/loja/orcamento/situacao/SituacaoOrcamento.java
@@ -1,4 +1,20 @@
 package com.ndrewcoding.loja.orcamento.situacao;
 
+import com.ndrewcoding.loja.exception.DomainException;
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
 public abstract class SituacaoOrcamento {
+
+    public void aprovar(Orcamento orcamento) {
+        throw new DomainException("Orçamento não pode ser aprovado!");
+    }
+
+    public void reprovar(Orcamento orcamento) {
+        throw new DomainException("Orçamento não pode ser reprovado!");
+    }
+
+    public void finalizar(Orcamento orcamento) {
+        throw new DomainException("Orçamento não pode ser finalizado!");
+    }
+
 }

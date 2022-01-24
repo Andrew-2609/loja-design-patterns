69003d27a382a3118387dca79eaab932de26aa8e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:17:18 -0300

    Feat: add SituacaoOrcamento as an attribute of Orcamento

--- src/com/ndrewcoding/loja/orcamento/Orcamento.java
+++ src/com/ndrewcoding/loja/orcamento/Orcamento.java
@@ -1,11 +1,14 @@
 package com.ndrewcoding.loja.orcamento;
 
+import com.ndrewcoding.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private final BigDecimal valor;
     private final int quantidadeItens;
+    private SituacaoOrcamento situacaoOrcamento;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
@@ -18,6 +21,14 @@ public class Orcamento {
 
     public int getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public SituacaoOrcamento getSituacaoOrcamento() {
+        return situacaoOrcamento;
+    }
+
+    public void setSituacaoOrcamento(SituacaoOrcamento situacaoOrcamento) {
+        this.situacaoOrcamento = situacaoOrcamento;
     }
 
 }

c563b63dbfae5ab8f373e416238a6fc7d08f5017
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:26:24 -0300

    Feat: override method finalizar() in Reprovado

--- src/com/ndrewcoding/loja/orcamento/situacao/Reprovado.java
+++ src/com/ndrewcoding/loja/orcamento/situacao/Reprovado.java
@@ -1,4 +1,12 @@
 package com.ndrewcoding.loja.orcamento.situacao;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
 public class Reprovado extends SituacaoOrcamento {
+
+    @Override
+    public void finalizar(Orcamento orcamento) {
+        orcamento.setSituacaoOrcamento(new Finalizado());
+    }
+
 }

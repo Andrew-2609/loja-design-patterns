ee371c695eb157f9718d687e1b2c7c01e8c03d53
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:20:54 -0300

    Feat: override methods aprovar() and reprovar() in EmAnalise

--- src/com/ndrewcoding/loja/orcamento/situacao/EmAnalise.java
+++ src/com/ndrewcoding/loja/orcamento/situacao/EmAnalise.java
@@ -1,4 +1,17 @@
 package com.ndrewcoding.loja.orcamento.situacao;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
 public class EmAnalise extends SituacaoOrcamento {
+
+    @Override
+    public void aprovar(Orcamento orcamento) {
+        orcamento.setSituacaoOrcamento(new Aprovado());
+    }
+
+    @Override
+    public void reprovar(Orcamento orcamento) {
+        orcamento.setSituacaoOrcamento(new Reprovado());
+    }
+
 }

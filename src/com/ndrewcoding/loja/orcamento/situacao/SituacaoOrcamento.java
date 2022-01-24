57dd720c5951e963416164d2422a14f6a22e879f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:24:19 -0300

    Feat: make calcularValorDescontoExtra() a method of SituacaoOrcamento

--- src/com/ndrewcoding/loja/orcamento/situacao/SituacaoOrcamento.java
+++ src/com/ndrewcoding/loja/orcamento/situacao/SituacaoOrcamento.java
@@ -3,7 +3,13 @@ package com.ndrewcoding.loja.orcamento.situacao;
 import com.ndrewcoding.loja.exception.DomainException;
 import com.ndrewcoding.loja.orcamento.Orcamento;
 
+import java.math.BigDecimal;
+
 public abstract class SituacaoOrcamento {
+
+    public BigDecimal calcularValorDescontoExtra(Orcamento orcamento) {
+        return BigDecimal.ZERO;
+    }
 
     public void aprovar(Orcamento orcamento) {
         throw new DomainException("Orçamento não pode ser aprovado!");

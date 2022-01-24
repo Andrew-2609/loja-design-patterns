6a8126ac538ecab3b8ec163f25565b164c31654d
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:25:14 -0300

    Feat: override methods calcularValorDescontoExtra() and finalizar() in Aprovado

--- src/com/ndrewcoding/loja/orcamento/situacao/Aprovado.java
+++ src/com/ndrewcoding/loja/orcamento/situacao/Aprovado.java
@@ -1,4 +1,19 @@
 package com.ndrewcoding.loja.orcamento.situacao;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class Aprovado extends SituacaoOrcamento {
+
+    @Override
+    public BigDecimal calcularValorDescontoExtra(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.02"));
+    }
+
+    @Override
+    public void finalizar(Orcamento orcamento) {
+        orcamento.setSituacaoOrcamento(new Finalizado());
+    }
+
 }

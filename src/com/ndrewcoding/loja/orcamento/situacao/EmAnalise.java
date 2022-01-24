3a917f7441559a53df01880f85679b8fa0062d77
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 20:21:19 -0300

    Feat: implement method calcularValorDescontoExtra() in EmAnalise

--- src/com/ndrewcoding/loja/orcamento/situacao/EmAnalise.java
+++ src/com/ndrewcoding/loja/orcamento/situacao/EmAnalise.java
@@ -2,7 +2,13 @@ package com.ndrewcoding.loja.orcamento.situacao;
 
 import com.ndrewcoding.loja.orcamento.Orcamento;
 
+import java.math.BigDecimal;
+
 public class EmAnalise extends SituacaoOrcamento {
+
+    public BigDecimal calcularValorDescontoExtra(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.05"));
+    }
 
     @Override
     public void aprovar(Orcamento orcamento) {

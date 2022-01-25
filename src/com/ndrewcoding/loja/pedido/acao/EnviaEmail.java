30ab27bd0ec6c35543c131c9eb4531248a9769fd
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:27:29 -0300

    Refactor: override methods executarAcao in classes of 'acao' package

--- src/com/ndrewcoding/loja/pedido/acao/EnviaEmail.java
+++ src/com/ndrewcoding/loja/pedido/acao/EnviaEmail.java
@@ -2,9 +2,10 @@ package com.ndrewcoding.loja.pedido.acao;
 
 import com.ndrewcoding.loja.pedido.Pedido;
 
-public class EnviaEmail {
+public class EnviaEmail implements AcaoAposGerarPedido {
 
-    public void executar(Pedido pedido) {
+    @Override
+    public void executarAcao(Pedido pedido) {
         System.out.println("Enviando e-mail com dados do pedido...");
     }
 
--- src/com/ndrewcoding/loja/pedido/acao/SalvaPedidoNoBancoDeDados.java
+++ src/com/ndrewcoding/loja/pedido/acao/SalvaPedidoNoBancoDeDados.java
@@ -2,9 +2,10 @@ package com.ndrewcoding.loja.pedido.acao;
 
 import com.ndrewcoding.loja.pedido.Pedido;
 
-public class SalvaPedidoNoBancoDeDados {
+public class SalvaPedidoNoBancoDeDados implements AcaoAposGerarPedido {
 
-    public void executar(Pedido pedido) {
+    @Override
+    public void executarAcao(Pedido pedido) {
         System.out.println("Salvando pedido no banco de dados...");
     }
 

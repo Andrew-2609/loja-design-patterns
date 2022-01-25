af92f1438dff396217bd5b5adab96107ca270805
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:06:08 -0300

    Refactor: move logic of executar() method of GeraPedido to GeraPedidoHandler

--- src/com/ndrewcoding/loja/pedido/GeraPedido.java
+++ src/com/ndrewcoding/loja/pedido/GeraPedido.java
@@ -1,9 +1,6 @@
 package com.ndrewcoding.loja.pedido;
 
-import com.ndrewcoding.loja.orcamento.Orcamento;
-
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class GeraPedido {
 
@@ -15,13 +12,6 @@ public class GeraPedido {
         this.cliente = cliente;
         this.valorOrcamento = valorOrcamento;
         this.quantidade = quantidade;
-    }
-
-    public void executar() {
-        Orcamento orcamento = new Orcamento(new BigDecimal("1000.0"), 4);
-        Pedido pedido = new Pedido(this.cliente, LocalDateTime.now(), orcamento);
-        System.out.println("Código para salvar pedido no banco de dados...");
-        System.out.println("Código para enviar e-mail com dados do novo pedido...");
     }
 
     public String getCliente() {
--- src/com/ndrewcoding/loja/pedido/GeraPedidoHandler.java
+++ src/com/ndrewcoding/loja/pedido/GeraPedidoHandler.java
@@ -1,4 +1,16 @@
 package com.ndrewcoding.loja.pedido;
 
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.time.LocalDateTime;
+
 public class GeraPedidoHandler {
+
+    public void executar(GeraPedido dadosPedido) {
+        Orcamento orcamento = new Orcamento(dadosPedido.getValorOrcamento(), dadosPedido.getQuantidade());
+        Pedido pedido = new Pedido(dadosPedido.getCliente(), LocalDateTime.now(), orcamento);
+        System.out.println("Código para salvar pedido no banco de dados...");
+        System.out.println("Código para enviar e-mail com dados do novo pedido...");
+    }
+
 }

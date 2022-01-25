3617c67be3e3d90c4e0ce1461db812cd49494faf
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:01:40 -0300

    Feat: create basic structure of GeraPedido

--- src/com/ndrewcoding/loja/pedido/GeraPedido.java
+++ src/com/ndrewcoding/loja/pedido/GeraPedido.java
@@ -1,4 +1,29 @@
 package com.ndrewcoding.loja.pedido;
 
+import java.math.BigDecimal;
+
 public class GeraPedido {
+
+    private final String cliente;
+    private final BigDecimal valorOrcamento;
+    private final int quantidade;
+
+    public GeraPedido(String cliente, BigDecimal valorOrcamento, int quantidade) {
+        this.cliente = cliente;
+        this.valorOrcamento = valorOrcamento;
+        this.quantidade = quantidade;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public BigDecimal getValorOrcamento() {
+        return valorOrcamento;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
 }

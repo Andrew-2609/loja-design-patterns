276c00b399f73f2ed70155c6b80242e5c19e804a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:30:58 -0300

    Refactor: create a List of AcaoAposGerarPedido and call it in a forEach loop in executar()

--- src/com/ndrewcoding/loja/pedido/GeraPedidoHandler.java
+++ src/com/ndrewcoding/loja/pedido/GeraPedidoHandler.java
@@ -1,22 +1,24 @@
 package com.ndrewcoding.loja.pedido;
 
 import com.ndrewcoding.loja.orcamento.Orcamento;
-import com.ndrewcoding.loja.pedido.acao.EnviaEmail;
-import com.ndrewcoding.loja.pedido.acao.SalvaPedidoNoBancoDeDados;
+import com.ndrewcoding.loja.pedido.acao.AcaoAposGerarPedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
+
+    private final List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
 
     public void executar(GeraPedido dadosPedido) {
         Orcamento orcamento = new Orcamento(dadosPedido.getValorOrcamento(), dadosPedido.getQuantidade());
         Pedido pedido = new Pedido(dadosPedido.getCliente(), LocalDateTime.now(), orcamento);
 
-        SalvaPedidoNoBancoDeDados salvaPedido = new SalvaPedidoNoBancoDeDados();
-        EnviaEmail email = new EnviaEmail();
-
-        salvaPedido.executar(pedido);
-        email.executar(pedido);
+        acoes.forEach(acao -> acao.executarAcao(pedido));
     }
 
 }

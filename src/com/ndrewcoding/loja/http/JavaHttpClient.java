f9f8ca07f663bd7e0b473e9b2b827c240addf4a2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-25 07:15:22 -0300

    Chore: create class JavaHttpClient implementing HttpAdapter

---
+++ src/com/ndrewcoding/loja/http/JavaHttpClient.java
@@ -0,0 +1,11 @@
+package com.ndrewcoding.loja.http;
+
+import java.util.Map;
+
+public class JavaHttpClient implements HttpAdapter {
+
+    @Override
+    public void post(String url, Map<String, Object> dados) {
+    }
+
+}

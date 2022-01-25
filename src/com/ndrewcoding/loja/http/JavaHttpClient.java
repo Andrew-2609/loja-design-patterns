f292fe0ee2cbbac5eed745ac675b42d1414f807a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-25 07:19:26 -0300

    Feat: override method post() in JavaHttpClient

--- src/com/ndrewcoding/loja/http/JavaHttpClient.java
+++ src/com/ndrewcoding/loja/http/JavaHttpClient.java
@@ -1,11 +1,20 @@
 package com.ndrewcoding.loja.http;
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 public class JavaHttpClient implements HttpAdapter {
 
     @Override
     public void post(String url, Map<String, Object> dados) {
+        try {
+            URL urlApi = new URL(url);
+            URLConnection connection = urlApi.openConnection();
+            connection.connect();
+        } catch (Exception e) {
+            throw new RuntimeException("Ocorreu um erro durante o envio da requisição HTTP:", e);
+        }
     }
 
 }

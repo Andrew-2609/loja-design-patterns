03f8375723ef6e6fba21e0b889672b49d25c2b47
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:08:00 -0300

    Feat: implement main() method of TestesPedidos

--- src/com/ndrewcoding/loja/TestesPedidos.java
+++ src/com/ndrewcoding/loja/TestesPedidos.java
@@ -1,15 +1,20 @@
 package com.ndrewcoding.loja;
 
-import com.ndrewcoding.loja.orcamento.Orcamento;
-import com.ndrewcoding.loja.pedido.Pedido;
+import com.ndrewcoding.loja.pedido.GeraPedido;
+import com.ndrewcoding.loja.pedido.GeraPedidoHandler;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class TestesPedidos {
 
     public static void main(String[] args) {
+        String cliente = args[0];
+        BigDecimal valorOrcamento = new BigDecimal(args[1]);
+        int quantidadeItens = Integer.parseInt(args[2]);
 
+        GeraPedido geraPedido = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+        GeraPedidoHandler geraPedidoHandler = new GeraPedidoHandler();
+        geraPedidoHandler.executar(geraPedido);
     }
 
 }

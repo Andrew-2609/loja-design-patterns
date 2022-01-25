23193e0cd7fbc65593c5b9e3c20f7647f2f4fa21
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-25 07:24:23 -0300

    Feat: add calls to recently implemented methods of RegistroDeOrcamento and JavaHttpClient

--- src/com/ndrewcoding/loja/TestesAdapters.java
+++ src/com/ndrewcoding/loja/TestesAdapters.java
@@ -1,4 +1,20 @@
 package com.ndrewcoding.loja;
 
+import com.ndrewcoding.loja.http.JavaHttpClient;
+import com.ndrewcoding.loja.orcamento.Orcamento;
+import com.ndrewcoding.loja.orcamento.RegistroDeOrcamento;
+
+import java.math.BigDecimal;
+
 public class TestesAdapters {
+    public static void main(String[] args) {
+
+        Orcamento orcamento = new Orcamento(BigDecimal.TEN, 1);
+        orcamento.aprovar();
+        orcamento.finalizar();
+
+        RegistroDeOrcamento registroDeOrcamento = new RegistroDeOrcamento(new JavaHttpClient());
+        registroDeOrcamento.registrar(orcamento);
+
+    }
 }

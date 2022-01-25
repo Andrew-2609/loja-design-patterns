10f939bf0d6a50e922a0f6bcac8a71d5c680cb51
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:32:38 -0300

    Chore: set a fixed value for variables of TestesPedidos

--- src/com/ndrewcoding/loja/TestesPedidos.java
+++ src/com/ndrewcoding/loja/TestesPedidos.java
@@ -11,9 +11,9 @@ import java.util.Arrays;
 public class TestesPedidos {
 
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Andrew Monteiro";
+        BigDecimal valorOrcamento = new BigDecimal("2000.0");
+        int quantidadeItens = 4;
 
         GeraPedido geraPedido = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
         GeraPedidoHandler geraPedidoHandler = new GeraPedidoHandler(

f11ee8a76f31a2aa53745586a819f516696aac61
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-24 21:21:47 -0300

    Feat: add very basic implementation of executar() methods of classes in the 'acao' package

--- src/com/ndrewcoding/loja/pedido/acao/EnviaEmail.java
+++ src/com/ndrewcoding/loja/pedido/acao/EnviaEmail.java
@@ -1,4 +1,11 @@
 package com.ndrewcoding.loja.pedido.acao;
 
+import com.ndrewcoding.loja.pedido.Pedido;
+
 public class EnviaEmail {
+
+    public void executar(Pedido pedido) {
+        System.out.println("Enviando e-mail com dados do pedido...");
+    }
+
 }
--- src/com/ndrewcoding/loja/pedido/acao/SalvaPedidoNoBancoDeDados.java
+++ src/com/ndrewcoding/loja/pedido/acao/SalvaPedidoNoBancoDeDados.java
@@ -1,4 +1,11 @@
 package com.ndrewcoding.loja.pedido.acao;
 
+import com.ndrewcoding.loja.pedido.Pedido;
+
 public class SalvaPedidoNoBancoDeDados {
+
+    public void executar(Pedido pedido) {
+        System.out.println("Salvando pedido no banco de dados...");
+    }
+
 }

8bfdbca2c7f88f6f9a6350d8320415411b712e12
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-26 07:17:13 -0300

    Feat: implement basic structure of OrcamentoProxy

--- src/com/ndrewcoding/loja/orcamento/OrcamentoProxy.java
+++ src/com/ndrewcoding/loja/orcamento/OrcamentoProxy.java
@@ -1,4 +1,23 @@
 package com.ndrewcoding.loja.orcamento;
 
-public class OrcamentoProxy {
+import java.math.BigDecimal;
+
+public class OrcamentoProxy extends Orcamento {
+
+    private BigDecimal valor;
+    private final Orcamento orcamento;
+
+    public OrcamentoProxy(BigDecimal valor, Orcamento orcamento) {
+        this.valor = valor;
+        this.orcamento = orcamento;
+    }
+
+    @Override
+    public BigDecimal getValor() {
+        if (valor == null) {
+            this.valor = orcamento.getValor();
+        }
+        return this.valor;
+    }
+
 }

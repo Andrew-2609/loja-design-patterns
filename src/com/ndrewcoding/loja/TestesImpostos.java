69013a24e96de08a03164168abb69515438fdfc4
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-26 06:50:29 -0300

    Refactor: adapt the implementation of Orcamento logic in TestesImpostos

--- src/com/ndrewcoding/loja/TestesImpostos.java
+++ src/com/ndrewcoding/loja/TestesImpostos.java
@@ -3,6 +3,7 @@ package com.ndrewcoding.loja;
 import com.ndrewcoding.loja.imposto.CalculadoraDeImpostos;
 import com.ndrewcoding.loja.imposto.ICMS;
 import com.ndrewcoding.loja.imposto.ISS;
+import com.ndrewcoding.loja.orcamento.ItemOrcamento;
 import com.ndrewcoding.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -10,7 +11,12 @@ import java.math.BigDecimal;
 public class TestesImpostos {
     public static void main(String[] args) {
 
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+        Orcamento orcamento = new Orcamento();
+
+        ItemOrcamento itemOrcamento = new ItemOrcamento(new BigDecimal("100"));
+
+        orcamento.adicionarItem(itemOrcamento);
+
         CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
         System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS(null)));
         System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS(new ICMS(null))));

dddc4a142d5b7d28261014f5c09d5d1f96e6e171
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-26 07:08:40 -0300

    Feat: implement AcaoAposGerarPedido in GeraLog and override its method

--- src/com/ndrewcoding/loja/pedido/acao/GeraLog.java
+++ src/com/ndrewcoding/loja/pedido/acao/GeraLog.java
@@ -1,2 +1,12 @@
-package com.ndrewcoding.loja.pedido.acao;public class GeraLog {
+package com.ndrewcoding.loja.pedido.acao;
+
+import com.ndrewcoding.loja.pedido.Pedido;
+
+public class GeraLog implements AcaoAposGerarPedido {
+
+    @Override
+    public void executarAcao(Pedido pedido) {
+        System.out.println("Pedido processado: " + pedido);
+    }
+
 }

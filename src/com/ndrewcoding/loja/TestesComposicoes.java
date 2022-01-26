23d2c1298e4646c5694be7131c837112090638d7
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-26 07:01:29 -0300

    Feat: implement new features of Orcamento in TestesComposicoes

--- src/com/ndrewcoding/loja/TestesComposicoes.java
+++ src/com/ndrewcoding/loja/TestesComposicoes.java
@@ -1,4 +1,22 @@
 package com.ndrewcoding.loja;
 
+import com.ndrewcoding.loja.orcamento.ItemOrcamento;
+import com.ndrewcoding.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class TestesComposicoes {
+    public static void main(String[] args) {
+
+        Orcamento orcamentoAntigo = new Orcamento();
+        orcamentoAntigo.adicionarItem(new ItemOrcamento(new BigDecimal("100")));
+        orcamentoAntigo.reprovar();
+
+        Orcamento orcamentoNovo = new Orcamento();
+        orcamentoNovo.adicionarItem(new ItemOrcamento(new BigDecimal("900")));
+        orcamentoNovo.adicionarItem(orcamentoAntigo);
+
+        System.out.println("Orçamentos combinados: " + orcamentoNovo.getValor());
+
+    }
 }

bd2db5f7fe4ebeb1d7f5cd62d23914ed941c4e75
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-26 06:51:37 -0300

    Refactor: apply new logic of Orcamento in TestesDescontos

--- src/com/ndrewcoding/loja/TestesDescontos.java
+++ src/com/ndrewcoding/loja/TestesDescontos.java
@@ -1,6 +1,7 @@
 package com.ndrewcoding.loja;
 
 import com.ndrewcoding.loja.desconto.CalculadoraDeDescontos;
+import com.ndrewcoding.loja.orcamento.ItemOrcamento;
 import com.ndrewcoding.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -8,8 +9,17 @@ import java.math.BigDecimal;
 public class TestesDescontos {
     public static void main(String[] args) {
 
-        Orcamento primeiroOrcamento = new Orcamento(new BigDecimal("100"), 6);
-        Orcamento segundoOrcamento = new Orcamento(new BigDecimal("1000"), 1);
+        Orcamento primeiroOrcamento = new Orcamento();
+        Orcamento segundoOrcamento = new Orcamento();
+
+        ItemOrcamento primeiroItem = new ItemOrcamento(new BigDecimal("500"));
+        ItemOrcamento segundoItem = new ItemOrcamento(new BigDecimal("550"));
+
+        primeiroOrcamento.adicionarItem(primeiroItem);
+        primeiroOrcamento.adicionarItem(segundoItem);
+
+        segundoOrcamento.adicionarItem(segundoItem);
+
         CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
         System.out.println(calculadoraDeDescontos.calcular(primeiroOrcamento));
         System.out.println(calculadoraDeDescontos.calcular(segundoOrcamento));
